5ac8a24f29a687aafe289634e311128d1c90eb22
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 17:13:13 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,22 @@
+# Fact
+import java.io.*;
+import java.util.*;
+public class Fact
+{
+  public static void main(String args[])
+  {
+    Scanner sc= new Scanner(System.in);
+System.out.println("Enter a number to find factorial"); 
+ 
+int n= sc.nextInt();
+int fact=1;
+
+for (int i = 1; i < n; i++) {
+  fact=fact*i;
+  
+}
+
+System.out.println("Factorial of "+n+" is "+fact);
+
+}
+}
